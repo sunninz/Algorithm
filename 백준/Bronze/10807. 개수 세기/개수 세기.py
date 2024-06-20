53a85162930a546298c6150affd227e4bce7e1c9
sunninz <129724474+sunninz@users.noreply.github.com>
2024-06-20 16:50:59 +0900

    [Bronze V] Title: 개수 세기, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,12 +1,8 @@
-import sys
 N=int(input())
-line=sys.stdin.readline()
-temp=[]
-temp.extend(map(int,line.split()))
+b=list(map(int,input().split()))
 v=int(input())
 count=0
-
-for i in range(len(temp)):
-    if temp[i]== v:
+for i in range(len(b)):
+    if b[i]== v:
         count+=1
 print(count)
