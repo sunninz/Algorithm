0d0a5bd8c8ad3ca36737616c3c5fc8bf5b215c1b
sunninz <129724474+sunninz@users.noreply.github.com>
2025-01-07 20:31:25 +0900

    [Bronze V] Title: 개수 세기, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,26 +1,34 @@
-import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int [] numbers = new int[n];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
         int count = 0;
+        int [] arr = new int[n];
 
-        for (int i = 0; i < n; i++){
-            int num = sc.nextInt();
-            numbers[i] =num;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for(int i = 0;i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int find = sc.nextInt();
-
-        for (int i = 0; i < n; i++){
-
-            if(find == numbers[i]){
-                count += 1;
+        
+        int find = Integer.parseInt(br.readLine());
+        
+        for(int j = 0; j < n; j++){
+            if(arr[j]==find){
+                count++;
             }
         }
 
         System.out.println(count);
+        
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
 }
