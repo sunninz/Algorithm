d059dbe4e4fae4a83db50e7bcdba89d00d52e076
sunninz <129724474+sunninz@users.noreply.github.com>
2025-01-09 13:08:57 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -6,24 +6,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter (System.out));
 
-
-        ArrayList <Integer> list = new ArrayList<>();
-
-        for(int i = 1; i <= 30; i++){
-            list.add(i);
+        //ArrayList <Integer> list = new ArrayList<>();
+        int [] arr = new int[30];
+        for(int i = 0; i < 30; i++){
+            arr[i] = i+1;
         }
 
 
         for (int j = 0; j < 28; j++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken())-1;
 
-            list.remove(Integer.valueOf(num));
-            
+            arr[num] = 0;
         }
 
-        for (int k = 0; k <list.size(); k++){
-            System.out.println(list.get(k));
+        for (int k = 0; k < 30; k++){
+            if(arr[k] != 0){
+                System.out.println(arr[k]);
+            }
+
         }
 
         br.close();
