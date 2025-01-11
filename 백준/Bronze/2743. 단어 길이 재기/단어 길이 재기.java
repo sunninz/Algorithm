2dbba9b568bcaa89a35666cff3e3d24bef7ebc08
sunninz <129724474+sunninz@users.noreply.github.com>
2025-01-11 12:25:42 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter (System.out));
+
+        String str = br.readLine();
+
+
+        System.out.println(str.length());
+
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+
+}
