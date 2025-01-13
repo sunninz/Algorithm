b7e0872d0bd6438ae51fdda776f7fefbf768c8c0
sunninz <129724474+sunninz@users.noreply.github.com>
2025-01-13 13:08:00 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 124 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int [] arr = {1,1,2,2,2,8};
+        int [] arrHave = new int[6];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < arr.length; i++){
+            arrHave[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i <arr.length; i++){
+            System.out.print(arr[i]-arrHave[i]+" ");
+        }
+
+
+    }
+
+}
