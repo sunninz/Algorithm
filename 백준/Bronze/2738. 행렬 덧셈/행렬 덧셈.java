4f86528531b1fdd0265e696e29fb65d6ef3c9ed3
kimsumin <129724474+sunninz@users.noreply.github.com>
2025-03-29 13:22:25 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 180 ms, Memory: 17556 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
@@ -12,34 +13,33 @@ public class Main{
         // 2차원 배열 선언
         int[][] first_array = new int[n][m];
         int[][] second_array = new int[n][m];
-        int[][] sum = new int[n][m];
+
+        //
         for(int i=0; i<n;i++){
-            st = new StringTokenizer(br.readLine());
+            String[] s = br.readLine().split(" ");
             for (int j=0;j<m;j++){
-                first_array[i][j]=Integer.parseInt(st.nextToken());
+                first_array[i][j]=Integer.parseInt(s[j]);
             }
         }
 
         for(int i=0; i<n;i++){
-            st = new StringTokenizer(br.readLine());
+            String[] s = br.readLine().split(" ");
             for (int j=0;j<m;j++){
-                second_array[i][j]=Integer.parseInt(st.nextToken());
+                second_array[i][j]=Integer.parseInt(s[j]);
             }
         }
 
 
         for(int i=0; i<n;i++){
             for (int j=0;j<m;j++){
-                sum[i][j] = first_array[i][j] + second_array[i][j];
+                bw.write((first_array[i][j] + second_array[i][j])+" ");
             }
+            bw.write("\n");
         }
-
-        for(int i=0; i<n;i++){
-            for (int j=0;j<m;j++){
-                System.out.print(sum[i][j]+" ");
-            }
-            System.out.println();
-        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
 
     }
 }
