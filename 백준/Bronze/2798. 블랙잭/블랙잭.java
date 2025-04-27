c22fffc2291fee3ff3fa4c49568885fa25f6cedb
Sumin <tnals00718@gmail.com>
2025-04-27 21:32:17 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -20,11 +20,12 @@ public class Main{
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
                 for(int k=j+1;k<n;k++){
-                    if(arr[i]+arr[j]+arr[k]>answer && arr[i]+arr[j]+arr[k]<=m){
-                        answer = arr[i]+arr[j]+arr[k];
+                    int temp = arr[i]+arr[j]+arr[k];
+                    if(temp>answer && temp<=m){
+                        answer = temp;
                     }
                 }
             }
