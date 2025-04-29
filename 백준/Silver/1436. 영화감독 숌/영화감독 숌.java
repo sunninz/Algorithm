e65629fafeacb79e238347b57a987bb0b82b991e
Sumin <tnals00718@gmail.com>
2025-04-30 00:50:18 +0900

    [Silver V] Title: 영화감독 숌, Time: 272 ms, Memory: 86668 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] argss) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        int number = 666;
+
+
+        while(true){
+            if(String.valueOf(number).contains("666")){
+                count++;
+            }
+            
+            if(count == n ){
+                bw.write(number+"");
+                break;
+            }
+
+            number++;
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+}
