ad549b8f607a29df697e9bc61e4cb843f5c0efef
Sumin <tnals00718@gmail.com>
2025-05-02 17:18:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -8,20 +8,20 @@ public class Main{
         
         int n = Integer.parseInt(br.readLine());
         boolean found = false;
-        for(int i = n/3; i >= 0; i--){
+        for(int i = n/5; i >= 0; i--){
             for(int j = n/3; j >= 0; j--){
                 if(5*i + 3*j == n){
                     bw.write((i+j)+"");
                     found = true;
                     break;
                 }
-
-                if(i==0 && j==0 && !found){
-                    bw.write("-1");
-                }
             }
             if(found) break;
             
+        }
+
+        if (!found) {
+            bw.write("-1\n");
         }
 
         br.close();
