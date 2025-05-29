9fe4cfed02b898a2e274d166f3260ed736f9503a
sumin <tnals00718@gmail.com>
2025-05-29 19:57:08 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 144 ms, Memory: 16436 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -9,18 +9,17 @@ public class Main{
 
         int [] arr = new int[26];
 
-        // 배열 초기화
+        // 알파벳 개수 카운트
         for(char c: str.toCharArray()){
             arr[c-'A']++;
         }
 
-        // 팰린드롬이 가능한지 확인
+        // 홀수 개수 체크 및 중앙 문자 설정
 
         int count = 0;
         char center = 0;
 
-        
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<26;i++){
             if(arr[i]%2==1){
                 count++;
                 center = (char) (i+'A');
@@ -29,39 +28,32 @@ public class Main{
             
         }
 
+        // 팰린드롬 가능 여부 확인
         if(count>1){
             System.out.println("I'm Sorry Hansoo");
             return;
         }
 
-        String pre ="", post="";
+        // 팰린드롬 만들기
 
-        int j=0;
-        while(j<26){
-            if(arr[j] !=0){
-                pre += (char) (j+'A');
-                arr[j] -=2;
-            }
+        // 앞쪽 구성
+        StringBuilder pre = new StringBuilder();
 
-            if(arr[j] == 0){
-                j++;
+        for (int i = 0; i < 26; i++) { //알파벳 A~Z
+            for (int j = 0; j < arr[i] / 2; j++) { // 알파벳의 개수 중 절만만큼 넣음
+                pre.append((char)(i + 'A'));
             }
         }
 
-        for(int i=pre.length()-1;i>=0;i--){
-        
-            post += pre.charAt(i);
-        }
-        
+        // 뒷쪽 구성
+        StringBuilder post = new StringBuilder(pre).reverse();
+
         // 결과 출력
-        
         if(count == 1){
-            System.out.println(pre+center+post);
+            System.out.println(pre.toString()+center+post.toString());
         }else{
-            System.out.println(pre+post);
+            System.out.println(pre.toString()+post.toString());
         }
-        
-
         
         br.close();
     }
