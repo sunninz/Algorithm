e7103372d96f707a7c09246cae4c5137ef7a9812
sumin <tnals00718@gmail.com>
2026-01-12 09:23:10 +0900

    [level 1] Title: 폰켓몬, Time: 4.47 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        HashMap <Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
         
-        // 초기화 (종류 번호, count)
+        // 초기화 (종류 번호)
         for(Integer n : nums){
-            map.put(n, map.getOrDefault(n,0) + 1);
+            set.add(n);
         }
         
         // 종류의 최댓값 찾기
         int answer = 0;
-        answer = Math.min(map.size(), nums.length/2);
+        answer = Math.min(set.size(), nums.length/2);
         
         return answer;
     }
